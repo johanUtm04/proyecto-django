8c02fa1d180afd9171deadf196c3fc621101c7ab
Johan Lopez <johanlopezrey1@gmail.com>
2025-08-06 09:24:25 -0600

    Guardando cambios antes de cambiar de commit

--- prueba/registros/migrations/0001_initial.py
+++ prueba/registros/migrations/0001_initial.py
@@ -13,7 +13,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.CreateModel(
             name='Alumnas',
-            name='Alumnos',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('matricula', models.CharField(max_length=12)),
